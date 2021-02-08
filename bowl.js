1a89a263ecba04981cbf83381f92b9413d8fc6fa
h510-cell <58731327+h510-cell@users.noreply.github.com>
2021-02-08 13:40:49 +0530

    Add files via upload

---
+++ bowl.js
@@ -0,0 +1,45 @@
+AFRAME.registerComponent("bowl", {
+    init: function () {
+      this.shootBowl();
+    },
+    shootBowl: function () {
+      window.addEventListener("keydown", (e) => {
+        if (e.key === "z") {
+          var bowl = document.createElement("a-entity");
+  
+          bowl.setAttribute("geometry", {
+            primitive: "sphere",
+            radius: 0.5,
+          });
+  
+          bowl.setAttribute("material", "color", "black");
+  
+          var cam = document.querySelector("#camera");
+  
+          pos = cam.getAttribute("position");
+  
+          bowl.setAttribute("position", {
+            x: pos.x,
+            y: pos.y,
+            z: pos.z,
+          });
+  
+          var camera = document.querySelector("#camera").object3D;
+  
+          //get the camera direction as Three.js Vector
+          var Direction = new THREE.Vector3();
+          camera.getWorldDirection(Direction);
+  
+          //set the velocity and it's direction
+          bowl.setAttribute("velocity", direction.multiplyScalar(-10));
+  
+          var scene = document.querySelector("#scene");
+  
+          scene.appendChild(bowl);
+        }
+      });
+    },
+  });
+  
+  
+  
